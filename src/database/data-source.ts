ac5bb07d35249144d906624b604d88f30322905d
BijanProgrammer <bijaneisapour@gmail.com>
2026-02-19 09:24:12 +0330

    [ADD] seed

---
+++ src/database/data-source.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { SeederOptions } from "typeorm-extension";
+
+const options: DataSourceOptions & SeederOptions = {
+  type: "postgres",
+  host: process.env.DATABASE_HOST!,
+  port: parseInt(process.env.DATABASE_PORT!),
+  username: process.env.DATABASE_USERNAME!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_DATABASE!,
+  entities: ["src/**/*.entity.ts"],
+};
+
+export const AppDataSource = new DataSource(options);
