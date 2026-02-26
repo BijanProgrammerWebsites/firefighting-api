09f811ae4aa40d69adf26a81d9581849312afe47
BijanProgrammer <bijaneisapour@gmail.com>
2026-02-26 10:09:06 +0330

    [ADD] truncate script

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -10,7 +10,6 @@ const options: DataSourceOptions & SeederOptions = {
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_DATABASE!,
   entities: ["src/**/*.entity.ts"],
-  seedTracking: true,
 };
 
 export const AppDataSource = new DataSource(options);
