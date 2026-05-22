5898955e389779794737b9b98647ecd9fdd9967d
BijanProgrammer <bijaneisapour@gmail.com>
2026-05-22 19:22:54 +0330

    [UPDATE] default refinery logo

--- src/database/seeds/refinery.seeder.ts
+++ src/database/seeds/refinery.seeder.ts
@@ -1,10 +1,27 @@
 import { DataSource } from "typeorm";
 import { Seeder } from "typeorm-extension";
 import { Refinery } from "../../refinery/entities/refinery.entity";
+import path from "node:path";
+import fs from "node:fs/promises";
 
 export class RefinerySeeder implements Seeder {
   async run(dataSource: DataSource) {
     const repo = dataSource.getRepository(Refinery);
-    await repo.save([{ title: "پالایشگاه اصلی", picture: null }]);
+
+    const fileStoragePath = process.env.FILE_STORAGE_PATH!;
+
+    const sourcePath = path.join(
+      process.cwd(),
+      "src/database/seeds/assets/sample-logo.png",
+    );
+
+    const destinationPath = path.join(fileStoragePath, "sample-logo.png");
+
+    await fs.mkdir(fileStoragePath, { recursive: true });
+    await fs.copyFile(sourcePath, destinationPath);
+
+    await repo.save([
+      { title: "پالایشگاه ستاره خلیج فارس", picture: "sample-logo.png" },
+    ]);
   }
 }
